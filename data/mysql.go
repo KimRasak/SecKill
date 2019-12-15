7ba76d8a2b1e05b60e64cfe06233dd77f5d5008a
karlhjm <1259040327@qq.com>
2019-12-15 11:40:49 +0800

    增加db初始化时失败重连尝试，防止docker里的db服务还没启动导致连接失败

--- data/mysql.go
+++ data/mysql.go
@@ -6,13 +6,15 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"time"
 )
 
 var Db *gorm.DB
 
 // 初始化连接，确保创建表、索引等
 func initMysql(config conf.AppConfig) {
-	fmt.Println("Load dbService config.")
+	fmt.Println("Load dbService config...")
 
 	// 设置连接相关的参数
 	dbType := config.App.Database.Type
@@ -23,12 +25,13 @@ func initMysql(config conf.AppConfig) {
 	dbLink := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		usr, pwd, address, dbName)
 
-	//创建一个数据库的连接
-	fmt.Println("Init dbService connections.")
+	//创建一个数据库的连接，因为docker中的mysql服务启动时延，一开始需要尝试重试连接
+	fmt.Println("Init dbService connections...")
 	var err error
-	Db, err = gorm.Open(dbType, dbLink)
-	if err != nil {
-		panic("failed to connect data: " + err.Error())
+	for Db, err = gorm.Open(dbType, dbLink); err != nil; Db, err = gorm.Open(dbType, dbLink) {
+		log.Println("Failed to connect database: ", err.Error())
+		log.Println("Reconnecting in 5 seconds...")
+		time.Sleep(5 * time.Second)
 	}
 
 	// 设置连接池连接数
